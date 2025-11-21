00c642331340e021bf46e78209516287cfbe64c4
Abdi Rahman Suufi <93520190+Abdi-Suufi@users.noreply.github.com>
2025-11-21 22:52:50 +0000

    Create Offer.js

---
+++ client/src/components/Offer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Offer = () => {
+  return (
+    <section className="offer section">
+      <div className="offer-bg">
+        <div className="offer-data">
+          <h2 className="offer-title">Special Offer</h2>
+          <p className="offer-description">Special offerts discounts for women this week only</p>
+          <a href="#" className="button">SHOP NOW</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Offer;
+
