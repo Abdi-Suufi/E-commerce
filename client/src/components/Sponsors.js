acc21c8773401cb0067a09b04767b3b560cc2c99
Abdi Rahman Suufi <93520190+Abdi-Suufi@users.noreply.github.com>
2025-11-26 14:55:48 +0000

    Create Sponsors.js

---
+++ client/src/components/Sponsors.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Sponsors = () => {
+  return (
+    <section className="sponsors section">
+      <div className="sponsors-container bd-grid">
+        <div className="sponsors-logo">
+          <img src="https://i.postimg.cc/3Jv5TRsx/logo1.png" alt="image logo" />
+        </div>
+
+        <div className="sponsors-logo">
+          <img src="https://i.postimg.cc/bJBWXcLn/logo2.png" alt="image logo" />
+        </div>
+
+        <div className="sponsors-logo">
+          <img src="https://i.postimg.cc/J0HgM6d2/logo3.png" alt="image logo" />
+        </div>
+
+        <div className="sponsors-logo">
+          <img src="https://i.postimg.cc/xjkTX9wW/logo4.png" alt="image logo" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Sponsors;
+
